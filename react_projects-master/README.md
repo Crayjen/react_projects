f9e30597238f5eb04663c3b6cc3d90ca03125ab1
Mitchell Jones <mitchell1997@gmail.com>
2019-03-22 15:43:13 -0400

    React tutorial

---
+++ react_projects-master/README.md
@@ -0,0 +1,2 @@
+# react_projects
+React projects and tutorials I am currently working on. Go into each projects folder from root and type 'npm start' to begin development server of react projects.
---
+++ react_projects-master/react_app_tutorial/my-app/src/index.js
@@ -0,0 +1,8 @@
+/* eslint-env browser */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './components/Game';
+import './index.css';
+
+ReactDOM.render(<Game />, document.getElementById('root'));
